52976ff944e59c7c558aee8409a312a451005483
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2025-03-19 15:19:35 +0530

    Initial commit with project files

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +9,15 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+=======
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Dockerized Python App!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+>>>>>>> 784e4ad (Initial commit with project files)
